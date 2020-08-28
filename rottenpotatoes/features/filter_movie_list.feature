647c43b2ce1352677bd51ebd61a3d62f1a8f18dd
jazzari <passionjazz@gmail.com>
2020-08-28 11:18:47 +0200

    Finishing BDD exercise

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -23,11 +23,14 @@ Background: movies have been added to database
   Then 10 seed movies should exist
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+  Given I am on the RottenPotatoes home page
+  When I check the following ratings: R, PG
+  When I uncheck the following ratings: PG-13, G, NC-17
+  When I press Refresh
+  Then I should see "PG" and "R" rated movies
+  Then I should not see "PG-13", "G", "NC-17" rated movies
 
 Scenario: all ratings selected
-  # see assignment
+  Given I am on the RottenPotatoes home page
+  When I check the following ratings: R, PG, PG-13, G, NC-17
+  Then I should see all of the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -15,24 +16,52 @@ end
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+Then /I should see "(.*)" before "(.*)"/ do |item1, item2|
   #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  page.body.should =~ /#{item1}.*#{item2}/m
+  #fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+When /I check the following ratings: (.*)/ do |rating_list|
   # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  ratings = rating_list.split(", ")
+  ratings.each do |rating|
+  check("ratings_#{rating}")
+  end
 end
 
-Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+When /I uncheck the following ratings: (.*)/ do |rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  ratings = rating_list.split(", ")
+  ratings.each do |rating|
+  uncheck("ratings_#{rating}")
+  end
+end
+
+When(/^I press Refresh$/) do
+  click_button('Refresh')
+end
+
+Then(/^I should see "([^"]*)" and "([^"]*)" rated movies$/) do |arg1, arg2|
+
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
+
+Then(/^I should not see "([^"]*)", "([^"]*)", "([^"]*)" rated movies$/) do |arg1, arg2, arg3|
+  page.body.should_not match(/<td>#{arg1}<\/td>/)
+  page.body.should_not match(/<td>#{arg2}<\/td>/)
+  page.body.should_not match(/<td>#{arg3}<\/td>/)
+end
+
+Then(/^I should see all of the movies$/) do
+  rows = page.all('tr#movies').size
+  expect(rows).to eq(10)
 end
